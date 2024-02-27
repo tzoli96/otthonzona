e9f049c992ccbca3d252ce5d6fc0e4f32e98b1c5
Your Name <test@asd.hu>
2024-02-27 07:54:37 +0100

    -- port 80

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-echo RUN prisma generate
+echo "RUN prisma generate"
 npx prisma generate
 
-echo RUN prisma db push
+echo "RUN prisma db push"
 npx prisma db push
 
-echo Start backend
-npm start server.js
+echo "Start backend"
+npm start server.js &
+echo "Backend started"
+
+# Wait for the backend to start before starting Nginx
+while ! nc -z localhost 5000; do
+  sleep 1
+done
+
+# Start Nginx
+nginx -g "daemon off;"
